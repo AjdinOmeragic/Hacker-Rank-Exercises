d49c02db2cba9f2415df202584508002d72dd705
Ajdin <ajdin.omeragic.pl@gmail.com>
2024-07-31 19:43:44 +0200

    Initial Commit

---
+++ C#Exercises-Hacker-Rank/HelloWorld.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello from VS Code");
+        Console.WriteLine ("Press 'Enter' ");
+        Console.ReadLine();
+        Console.WriteLine ("I am Learining C#");
+    }
+}
